9635e15c249c6dd6107297ec5a71a91e59ecf4e6
cle99y <chilemerlot@gmail.com>
2017-02-02 18:11:33 +0000

    Splash screen

--- app/src/main/java/com/clackityclack/lxxcroft/Splash.java
+++ app/src/main/java/com/clackityclack/lxxcroft/Splash.java
@@ -3,6 +3,8 @@ package com.clackityclack.lxxcroft;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.widget.TextView;
 
 /**
  * Created by cle99 on 02/02/2017.
@@ -17,20 +19,42 @@ public class Splash extends Activity {
 
         Thread splash = new Thread () {
 
-            public void run() {
+            String sLoad = "Loading";
+            float currentSize = 20;
+            float startSize = 20;
+            float endSize = 35;
 
+            public int cycle = 0;
+            TextView load = (TextView) findViewById ( R.id.loading );
+            TextView splash70 = (TextView) findViewById ( R.id.splash );
+
+            public void run() {
                 try {
-                    sleep ( 2500 );
+                    while (cycle < 100) {
+                        Thread.sleep ( 25 );
+                        runOnUiThread ( new Runnable () {
+                            @Override
+                            public void run() {
+                                load.setText ( sLoad );
+                                splash70.setTextSize ( TypedValue.COMPLEX_UNIT_SP, currentSize );
+                                currentSize += (endSize - startSize) / 100;
+                                if (cycle % 20 == 0) {
+                                    sLoad += ".";
+                                }
+                                cycle += 1;
+                            }
+                        });
+                    }
                 } catch (InterruptedException e) {
                     e.printStackTrace ();
                 } finally {
                     Intent startMain = new Intent ( Splash.this, MainActivity.class );
                     startActivity ( startMain );
                 }
-
             }
-
         };
         splash.start ();
+
     }
 }
+
